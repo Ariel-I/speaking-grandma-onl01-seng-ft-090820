63f8706cd121e1d2882ee2af18cb26d38f11e59a
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-09 23:45:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ if phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
 elsif phrase != phrase.upcase
 	return "HUH?! SPEAK UP, SONNY!"
-else phrase == "I LOVE YOU GRANDMA!"
+elsif phrase == "I LOVE YOU GRANDMA!"
 	return "I LOVE YOU TOO PUMPKIN!"
 end
 end
