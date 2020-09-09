f81c1bd3ee851da0972bb7155ae81273f0e740dd
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-09 22:24:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
-def speak_to_grandma (phrase)
+def speak_to_grandma (phrase.upcase)
   
-if speak_to_grandma == "phrase.upcase"
+if phrase.upcase == "phrase.upcase"
   puts "NO, NOT SINCE 1938!"
-elsif speak_to_grandma != "phrase.upcase"
+elsif phrase.upcase != "phrase.upcase"
   puts "HUH?! SPEAK UP SONNY!"
-else speak_to_grandma == "I LOVE YOU GRANDMA"
+else phrase.upcase == "I LOVE YOU GRANDMA"
   puts "I LOVE YOU TOO PUMPKIN!"
 end
 end
