88eba58d4205dfa11d588c33db07cb154abd4925
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-09 21:22:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
-speak_to_grandma = "I LOVE YOU GRANDMA!".upcase
+speak_to_grandma = "I LOVE YOU GRANDMA!"
 
-if speak_to_grandma == "hi".downcase
+if speak_to_grandma == "hi Nana, how are you?"
   puts "HUH?! SPEAK UP SONNY!"
-elsif speak_to_grandma == "I LOVE YOU GRANDMA!".upcase
+elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-else speak_to_grandma == "HI".upcase
+else speak_to_grandma == "HI"
   puts "NO, NOT SINCE 1938!"
 
 end
