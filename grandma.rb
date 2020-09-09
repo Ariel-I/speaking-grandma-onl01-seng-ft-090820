6a25dc87dba66b776df3f52ebfba7981874b0878
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-09 21:13:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 speak_to_grandma = "upcase"
 
-if speak_to_grandma == "upcase"
+if speak_to_grandma == "downcase"
   puts "HUH?! SPEAK UP SONNY!"
 elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
