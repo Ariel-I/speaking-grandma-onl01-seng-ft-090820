6aa39958577549e1075f2a679654142b0fbf6e90
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-09 21:36:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,7 @@
 def speak_to_grandma (phrase)
 
-if speak_to_grandma == phrase.uppcase
-  puts "NO, NOT SINCE 1938!"
-else
-  puts "HUH?! SPEAK UP SONNY!"
+if phrase == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
 end
 
 # Write a speak_to_grandma method.
